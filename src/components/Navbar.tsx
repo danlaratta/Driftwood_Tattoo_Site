d8aed525bebe47c2fcc9d3adbc984277838feddd
Dan <dan.laratta.jr@gmail.com>
2023-06-15 15:25:29 -0400

    navbar work

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,60 +1,53 @@
 'use client';
 
-import Image from "next/image";
-import Link from "next/link";
-import logo from '../../public/logo.png';
+import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
-import { useState } from "react";
+
 
 const Navbar = () => {
-   const [menuOpen, setMenuOpen] = useState(false)
-
-   const handleMenu = () => {
-         setMenuOpen(!menuOpen)
-   }
-
-  return (
-    <nav className="w-full fixed z-[100] bg-[#ecf0f3] shadow-xl h-20">
-        <div className="flex items-center sm:justify-center justify-between h-full w-full">
-            <Link href="/">
-                <Image 
-                src={logo}
-                alt="logo"
-                width='305'
-                height='305'
-                priority 
-                />
+    return (
+        <nav className="bg-red-500">
+            <div className="w-10/12-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <Link href="https://flowbite.com/" className="flex items-center">
+                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
                 </Link>
-                <div className="mr-6">
-   <AiOutlineMenu onClick={handleMenu} className="sm:hidden text-2xl cursor-pointer" />
+
+                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none        
+                        focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                </button>
+
+                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <Link href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
+                        </li>
+
+                        <li>
+                            <Link href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</Link>
+                        </li>
+
+                        <li>
+                            <Link href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</Link>
+                        </li>
+
+                        <li>
+                            <Link href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</Link>
+                        </li>
+
+                        <li>
+                            <Link href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
+                        </li>
+                    </ul>
                 </div>
-        </div>
-        {/* Mobile Version Listed Items */}
-        <div className={
-            menuOpen ? 'left-0 top-0 w-[65%] h-screen relative bg-[#ecf0f3] ease-in duration-500 flex flex-col justify-start z-50': 'hidden'
-        }>
-            <div className="flex absolute top-6 right-6 w-full justify-end items-center">
-                <AiOutlineClose onClick={handleMenu} className="text-2xl text-black mb-4" />
             </div>
-            <ul className="flex flex-col justify-center items-center mt-20 ">
-                <li className="text-2xl text-black mb-4">
-                    <Link href="/"> Home</Link>
-                </li>
-                <li className="text-2xl text-black mb-4">
-                    <Link href="/pricing"> Pricing</Link>
-                </li>
-                <li className="text-2xl text-black mb-4">
-                    <Link href="/contact"> Contact</Link>
-                </li>
-                <li className="text-2xl text-black mb-4">
-                    <Link href="/portfolio"> Portfolio</Link>
-                </li>
-            </ul>
+        </nav>
 
-        </div>
-
-    </nav>
-  )
+    );
 }
 
 export default Navbar;
