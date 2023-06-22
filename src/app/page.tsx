b9ac58c868050cf353bb8a0e4286384d1ae8f95d
Dan <dan.laratta.jr@gmail.com>
2023-06-22 16:35:46 -0400

    LinkCards work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Hero from '@/components/Hero'
+import LinkCards from '@/components/LinkCards'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
-      <div className="w-full h-screen bg-red-300"></div>
+      
+      <div className="w-full flex justify-center">
+        <LinkCards img={'/artists.jpg'} link={'/artists'} text={'Artists'} />
+      </div>
+        <div className="w-full h-screen bg-red-300"></div>
     </main>
   )
 }
--- src/components/LinkCards.tsx
+++ src/components/LinkCards.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Link from 'next/link';
+import { motion, useAnimation } from 'framer-motion';
 import Image from 'next/image';
 
 type LinkCardProps = {
@@ -8,14 +10,37 @@ type LinkCardProps = {
 }
 
 const LinkCards = ({img, link, text}:LinkCardProps ) => {
-    return (
-        <div className="">
-            <Link href={link}>
-                <Image src={img} alt={`${text} link image`} />
+    const controls = useAnimation();
 
-                <span className="">
-                    {text}
-                </span>
+    const handleHoverStart = () => {
+        controls.start({ height: '100%' });
+    };
+
+    const handleHoverEnd = () => {
+        controls.start({ height: '10%' });
+    };
+
+    return (
+        <div className="relative">
+            <Link href={link} >
+                <motion.div
+                    className="relative"
+                    onHoverStart={handleHoverStart}
+                    onHoverEnd={handleHoverEnd}
+                >
+                    <Image src={img} alt={`${text} link image`} width={350} height={450} />
+
+                    <motion.div
+                    initial={{ height: '10%' }}
+                    animate={controls}
+                    transition={{ duration: 0.3 }}
+                    className="w-full flex justify-center items-center absolute bottom-0 bg-black/60"
+                    >
+                        <span className="text-white">
+                            {text}
+                        </span>
+                    </motion.div>
+                </motion.div>
             </Link>
         </div>
     );
